5d80192b732bdf240f636744e62c10dfe6b97e82
sorecauadrian <a.sorecau@aico.swiss>
2024-07-03 17:04:57 +0300

    episode18

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -15,12 +15,15 @@ class CommentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id"=> $this->id,
+            'id' => $this->id,
             'user' => $this->whenLoaded('user', fn () => UserResource::make($this->user)),
             'post' => $this->whenLoaded('post', fn () => PostResource::make($this->post)),
-            'body'=> $this->body,
-            'updated_at'=> $this->updated_at,
-            'created_at'=> $this->created_at
+            'body' => $this->body,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+            'can' => [
+                'delete' => $request->user()?->can('delete', $this->resource),
+            ],
         ];
     }
 }
