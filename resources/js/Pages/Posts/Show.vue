b6758f486d74b1c4b12741d3bedc13e654bcbec3
sorecauadrian <a.sorecau@aico.swiss>
2024-07-03 16:04:38 +0300

    episode15

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,17 +1,29 @@
 <template>
     <AppLayout :title="post.title">
         <Container>
-            <h1 class="text=2xl font-bold">{{ post.title }}</h1>
+            <h1 class="text-2xl font-bold">{{ post.title }}</h1>
             <span class="block mt-1 text-sm text-gray-600">{{ formattedDate }} ago by {{ post.user.name }}</span>
+
             <article class="mt-6">
                 <pre class="whitespace-pre-wrap font-sans">{{ post.body }}</pre>
             </article>
 
             <div class="mt-12">
                 <h2 class="text-xl font-semibold">Comments</h2>
+
+                <form v-if="$page.props.auth.user" @submit.prevent="addComment" class="mt-4">
+                    <div>
+                        <InputLabel for="body" class="sr-only">Comment</InputLabel>
+                        <TextArea id="body" v-model="commentForm.body" rows="4" placeholder="Speak your mind Spock…"/>
+                        <InputError :message="commentForm.errors.body" class="mt-1" />
+                    </div>
+
+                    <PrimaryButton type="submit" class="mt-3" :disabled="commentForm.processing">Add Comment</PrimaryButton>
+                </form>
+
                 <ul class="divide-y mt-4">
                     <li v-for="comment in comments.data" :key="comment.id" class="px-2 py-4">
-                        <Comment :comment="comment" />
+                        <Comment :comment="comment"/>
                     </li>
                 </ul>
 
@@ -22,14 +34,30 @@
 </template>
 
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Container from '@/Components/Container.vue';
-import Pagination from '@/Components/Pagination.vue';
-import Comment from '@/Components/Comment.vue';
-import { defineProps, computed } from 'vue';
-import { formatDistance, parseISO } from 'date-fns';
-import { relativeDate } from '@/Utilities/date.js';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import {computed} from "vue";
+import {formatDistance, parseISO} from "date-fns";
+import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
+import {relativeDate} from "@/Utilities/date.js";
+import Comment from "@/Components/Comment.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import {useForm} from "@inertiajs/vue3";
+import TextArea from "@/Components/TextArea.vue";
+import InputError from "@/Components/InputError.vue";
 
 const props = defineProps(['post', 'comments']);
+
 const formattedDate = computed(() => relativeDate(props.post.created_at));
+
+const commentForm = useForm({
+    body: '',
+});
+
+const addComment = () => commentForm.post(route('posts.comments.store', props.post.id), {
+    preserveScroll: true,
+    onSuccess: () => commentForm.reset(),
+});
 </script>
