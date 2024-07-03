636454d1c366fbf3457113a0f3838c0f7afded4d
sorecauadrian <a.sorecau@aico.swiss>
2024-07-03 16:49:29 +0300

    episode17

--- routes/web.php
+++ routes/web.php
@@ -40,9 +40,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
+    Route::delete('comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 });
 
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-
-Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
