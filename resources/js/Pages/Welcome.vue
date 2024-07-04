212964940f64aec468099248cc21e8be1f5b9a50
sorecauadrian <a.sorecau@aico.swiss>
2024-07-04 15:18:13 +0300

    changed the welcome page and got rid of email verification

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -27,8 +27,12 @@ defineProps({
             <div class="flex justify-center">
                 <img src="/assets/logo.png" class="h-24"/>
             </div>
+            
+            <div class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed items-center justify-center text-center">
+                <p>all about shrek</p>
+            </div>
 
-            <div class="mt-16">
+            <!-- <div class="mt-16">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div>
@@ -106,7 +110,7 @@ defineProps({
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
             <div class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
                 <div class="text-center text-sm sm:text-start">
